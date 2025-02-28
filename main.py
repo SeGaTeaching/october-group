a838ab17af5575d022f1bb0eff6f7b0791e19192
Sebastiano di Gauso <sebastian.gaus@enders-training.de>
2025-02-28 08:55:33 +0100

    Commit in feature_3 - very sophisticated python code added

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from tools import add
+
 class Person:
     def __init__(self, name, age):
         self.name = name
@@ -7,3 +9,8 @@ class Person:
         return self.name
 
 person_1 = Person("Heike", 33)
+person_2 = Person("Uwe", 54)
+person_3 = Person("Mathilde", 77)
+
+personen_alter = add(person_1.age, person_2.age, person_3.age)
+print(personen_alter)
